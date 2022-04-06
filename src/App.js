29d038cec4c3952f440e9bed3868e9235c83b51c
SHASHWAT TULSYAN <shashwattulsyan123@gmail.com>
2022-04-06 19:59:45 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import Title from './title';
 
 function App() {
+  
+  const[count,setCount]=useState(0)
+  useEffect(()=>
+  {
+    document.title=`messages ${count}`;
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <>
+    <div className=''>
+    <button>count={count}</button>
+    <button onClick={()=>setCount(count+1)}>Increase</button>
+
+    <button onClick={()=>setCount(count-1)}>Decrease</button>
+    <button onClick={()=>setCount(0)}>reset</button>
     </div>
+    </>
   );
 }
 
